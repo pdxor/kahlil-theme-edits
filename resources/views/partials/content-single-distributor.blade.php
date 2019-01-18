f0fdd1149b6de4e1d3b24e4e16f2e6ff0c1a46b3
pdxor <kahlilcalavas@gmail.com>
2019-01-18 12:56:44 -0500

    still progressing with acf

--- resources/views/partials/content-single-distributor.blade.php
+++ resources/views/partials/content-single-distributor.blade.php
@@ -6,8 +6,33 @@
   </div>
   <div class="col-sm-4">
     <p><img class="img-fluid" src="<?php the_field('sales_manager_picture'); ?>"></p>
+    <strong><?php the_field('sales_manager_name'); ?></strong> <a href="mailto:<?php the_field('sales_email_address'); ?>">Contact Me</a>
   </div>
 </div>
+<div class="row">
+  <div class="col-sm-8">
+    <div class="col-sm-8">
+      <h2><?php the_field('media_section_title'); ?></h2>
+      <div class="row">
+        <div class="col-sm-3">
+              <p>links here</p>
+              <p>links here</p>
+        </div>
+        <div class="col-sm-9">
+          <p>links here</p>
+          <p>links here</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <h2><?php the_field('content_links_section_title'); ?></h2>
+      <p>links here</p>
+      <p>links here</p>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <a href="<?php the_field('digital_ad_link'); ?>"><img class="img-fluid" src="<?php the_field('digital_ad_image'); ?>"></a>
+  </div>
 </div>
 <article @php(post_class('row'))>
   <div class="entry-content col-md-12">
