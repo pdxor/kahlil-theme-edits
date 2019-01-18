42af234404a212379469ed4d772a779302189b9e
pdxor <kahlilcalavas@gmail.com>
2019-01-18 16:22:40 -0500

    added all fields styles need to be imported

--- resources/views/partials/content-single-distributor.blade.php
+++ resources/views/partials/content-single-distributor.blade.php
@@ -9,31 +9,30 @@
     <strong><?php the_field('sales_manager_name'); ?></strong> <a href="mailto:<?php the_field('sales_email_address'); ?>">Contact Me</a>
   </div>
 </div>
+<!-- row  with ad and links begin -->
 <div class="row">
+  <!-- media column begin -->
   <div class="col-sm-8">
-    <div class="col-sm-8">
-      <h2><?php the_field('media_section_title'); ?></h2>
-      <div class="row">
-        <div class="col-sm-3">
-              <p>links here</p>
-              <p>links here</p>
-        </div>
-        <div class="col-sm-9">
-          <p>links here</p>
-          <p>links here</p>
-        </div>
+    <div class="row">
+      <div class="col-sm-8">
+        <h2><?php the_field('media_section_title'); ?></h2>
+        <p>links here</p>
+        <p>links here</p>
+      </div>
+      <div class="col-sm-4">
+        <h2><?php the_field('content_links_section_title'); ?></h2>
+        <p>links here</p>
+        <p>links here</p>
       </div>
     </div>
-    <div class="col-sm-4">
-      <h2><?php the_field('content_links_section_title'); ?></h2>
-      <p>links here</p>
-      <p>links here</p>
-    </div>
   </div>
+
   <div class="col-sm-4">
     <a href="<?php the_field('digital_ad_link'); ?>"><img class="img-fluid" src="<?php the_field('digital_ad_image'); ?>"></a>
   </div>
+
 </div>
+<!-- row  with ad and links end -->
 <article @php(post_class('row'))>
   <div class="entry-content col-md-12">
     @php(the_content())
---
+++ resources/views/partials/footer/dist-resources.blade.php
@@ -0,0 +1,66 @@
+@push('footer')
+<div class="container resourceblocks">
+  <h2>Distributor Resources</h2>
+  <div class="row">
+    <div class="col-sm-3">
+       <p><?php the_field('dist_res_col1'); ?></p>
+    </div>
+    <div class="col-sm-3">
+      <p><?php the_field('dist_res_col2'); ?></p>
+    </div>
+    <div class="col-sm-3">
+      <p><?php the_field('dist_res_col3'); ?></p>
+    </div>
+    <div class="col-sm-3">
+    <p><p><?php the_field('dist_res_col4'); ?></p></p>
+    </div>
+  </div>
+</div>
+<div class="container featapptext">
+  <div class="row">
+    <p><h2 class="centerme">Featured MicroCare Electronics Cleaning Applications</h2>
+      Modern electronics are omnipresent in our world, making life better for nearly every person on the planet. But PCBs need to be properly cleaned to perform as expected. MicroCare has both the products and the know-how to get your boards perfectly clean, maximizing their potential and minimizing costs.
+      <a class="centerme" href="#">See All Applications</a>
+    </p>
+
+  </div>
+</div>
+<div class="container featappblocks">
+
+  <div class="row">
+    <div class="col-sm-4">
+       <p><?php the_field('featured_cleaning1'); ?></p>
+    </div>
+    <div class="col-sm-4">
+      <p><?php the_field('featured_cleaning2'); ?></p>
+    </div>
+    <div class="col-sm-4">
+      <p><?php the_field('featured_cleaning3'); ?></p>
+    </div>
+  </div>
+</div>
+<div class="container cclblock">
+  <div class="row">
+    <div class="col-sm-3">
+      <p><?php the_field('ccl_link_box1'); ?></p>
+    </div>
+    <div class="col-sm-3">
+      <p><?php the_field('ccl_link_box2'); ?></p>
+    </div>
+    <div class="col-sm-3">
+      <p><?php the_field('ccl_link_box3'); ?></p>
+    </div>
+    <div class="col-sm-3">
+      <p><?php the_field('ccl_link_box4'); ?></p>
+    </div>
+  </div>
+</div>
+<div class="container confidentaltext">
+  <div class="row">
+    <h2>Confidentiality</h2>
+
+    <p>Modern electronics are omnipresent in our world, making life better for nearly every person on the planet. But PCBs need to be properly cleaned to perform as expected. MicroCare has both the products and the know-how to get your boards perfectly clean, maximizing their potential and minimizing costs.</p>
+
+  </div>
+</div>
+@endpush
